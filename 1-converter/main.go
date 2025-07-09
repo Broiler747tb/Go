678e09d472a66873265763e8db43aad7a39cd412
Daniel Kosovskiy <churkabes420@gmail.com>
2025-07-09 16:41:43 +0300

    Save of progress

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,17 +7,54 @@ const (
 	usdrub = 78.17
 )
 
+var eurrub = usdrub / usdeur
+
 func main() {
-	var eurrub = usdrub / usdeur
-	fmt.Println(eurrub)
+	for {
+		gog := readcurinput()
+		if gog == "Error: No such currency, try again." {
+			fmt.Println("Error: No such currency, try again.")
+			continue
+		}
+		ts :=
+	}
 }
 
-func readinput() string {
+func readcurinput() string {
 	var sc string
 	fmt.Scan(&sc)
-	return sc
+	if sc == "eur" || sc == "rub" || sc == "usd" {
+		return sc
+	} else {
+		return "Error: No such currency, try again."
+	}
 }
 
-func cunt(int, string, string) int {
-	return 0
+func readnumbinput() float64 {
+	var numb float64
+	fmt.Scan(&numb)
+	
+	return numb
+}
+
+func converter(numb float64, start string, finish string) float64 {
+	if start == "usd" && finish == "eur" {
+		return numb * usdeur
+	}
+	if start == "usd" && finish == "rub" {
+		return numb * usdrub
+	}
+	if start == "eur" && finish == "rub" {
+		return numb * eurrub
+	}
+	if finish == "usd" && start == "eur" {
+		return numb / usdeur
+	}
+	if finish == "usd" && start == "rub" {
+		return numb / usdrub
+	}
+	if finish == "eur" && start == "rub" {
+		return numb / eurrub
+	}
+	return 0.0
 }
